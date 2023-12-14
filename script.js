bc1f706b86752406a06153006659e70312897c4f
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-14 15:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function fibonacci(num) {
 // your code here
+if (num === 0) {
+    return 0;//0th fib
+  } else if (num === 1) {//1st fib
+    return 0; // If you consider the sequence to start with 0, as per the example
+  } else if (num === 2) {//2nd fib
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    let fib = 0;//do swaping
+    
+    for (let i = 2; i < num; i++) {
+      fib = a + b;
+      a = b;
+      b = fib;
+    }
+    
+    return fib;
+  }
 }
 
 module.exports = fibonacci;
